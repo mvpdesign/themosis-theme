71ad3c0e8e4346472348d8108ef43ea8759f1150
Brock Riemenschneider <brock@mvpdesign.com>
2015-01-16 10:45:50 -0600

    Refactored app dispatcher

--- app/assets/src/scripts/dispatchers/AppDispatcher.js
+++ app/assets/src/scripts/dispatchers/AppDispatcher.js
@@ -1,19 +1,5 @@
 var Dispatcher = require('flux').Dispatcher;
 
-var AppDispatcher = assign(new Dispatcher(), {
-
-  /**
-   * A bridge function between the views and the dispatcher, marking the action
-   * as a view action.  Another variant here could be handleServerAction.
-   * @param  {object} action The data coming from the view.
-   */
-  handleViewAction: function(action) {
-    this.dispatch({
-      source: 'VIEW_ACTION',
-      action: action
-    });
-  }
-
-});
+var AppDispatcher = anew Dispatcher();
 
 module.exports = AppDispatcher;
