825218b4fb15e92d932ea07d5e7686bd96d940ef
Brock Riemenschneider <brock@mvpdesign.com>
2015-01-20 16:41:23 -0600

    Added option to generate base64 svg and retireve absolute path

--- app/models/theme/Assets.php
+++ app/models/theme/Assets.php
@@ -63,50 +63,91 @@ class Assets
     /**
      * Return a path to the css assets directory
      *
+     * @param  string  $path
+     * @param  boolean $absolutePath
      * @return string
      */
-    public function css($path = '')
+    public function css($path = '', $absolutePath = false)
     {
-        return $this->path($this->cssDir . '/' . $path);
+        return $this->path($this->cssDir . '/' . $path, $absolutePath);
     }
 
     /**
      * Return a path to the images assets directory
      *
+     * @param  string  $path
+     * @param  boolean $absolutePath
      * @return string
      */
-    public function image($path = '')
+    public function image($path = '', $absolutePath = false)
     {
-        return $this->path($this->imagesDir . '/' . $path);
+        return $this->path($this->imagesDir . '/' . $path, $absolutePath);
     }
 
     /**
      * Return a path to the js assets directory
      *
+     * @param  string  $path
+     * @param  boolean $absolutePath
      * @return string
      */
-    public function js($path = '')
+    public function js($path = '', $absolutePath = false)
     {
-        return $this->path($this->jsDir . '/' . $path);
+        return $this->path($this->jsDir . '/' . $path, $absolutePath);
     }
 
     /**
      * Return a path to the fonts assets directory
      *
+     * @param  string  $path
+     * @param  boolean $absolutePath
      * @return string
      */
-    public function font($path = '')
+    public function font($path = '', $absolutePath = false)
     {
-        return $this->path($this->fontsDir . '/' . $path);
+        return $this->path($this->fontsDir . '/' . $path, $absolutePath);
     }
 
     /**
      * Returns the path to the assets directory
      *
+     * @param  string  $path
+     * @param  boolean $absolutePath
      * @return string
      */
-    public function path($path = '')
+    public function path($path = '', $absolutePath = false)
+    {
+        return $absolutePath ? $this->absolutePath($path) : $this->uriPath($path);
+    }
+
+    /**
+     * Returns the URI path to the assets directory
+     *
+     * @return string
+     */
+    public function uriPath($path = '')
     {
         return $this->theme->getTemplateDirectoryURI() . '/' . $this->assetsDir . '/' . $this->distDir . '/' . $path;
+    }
+
+    /**
+     * Returns a base64 encoded string of an svg
+     *
+     * @return string
+     */
+    public function svg($path = '')
+    {
+        $contents = @file_get_contents($path);
+        return $contents ? 'data:image/svg+xml;base64,' . base64_encode($contents) : false;
+    }
+
+    /**
+     * Returns the absolute path to the assets directory
+     *
+     * @return string
+     */
+    public function absolutePath($path = '')
+    {
+        return $this->theme->getTemplateDirectory() . '/' . $this->assetsDir . '/' . $this->distDir . '/' . $path;
     }
 }
